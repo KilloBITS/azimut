bec56466bd89da48621257780a7cb8582147bead
mrkalinuk <mr.kalinuk@gmail.com>
2019-03-06 18:00:50 +0200

    Your Message

--- viber/viberController.js
+++ viber/viberController.js
@@ -9,27 +9,27 @@ const express = require('express');
 const app = express();
 
 
-// var bot = new ViberBot({
-// 	authToken: '4954bf9dc4e7d1f7-63f14c65d241b347-b3ec6f1293615a66',
-// 	name: "Azimuth NEWS",
-// 	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
-// });
-// bot.setWebhook("https://chatapi.viber.com/pa/set_webhook").then(() => yourBot.doSomething()).catch(err => console.log(err));
+var bot = new ViberBot({
+	authToken: '4954bf9dc4e7d1f7-63f14c65d241b347-b3ec6f1293615a66',
+	name: "Azimuth NEWS",
+	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
+});
+bot.setWebhook("https://chatapi.viber.com/pa/set_webhook").then(() => yourBot.doSomething()).catch(err => console.log(err));
 
 
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-// 	response.send(`Thanks for subscribing, ${response.userProfile.name}`)
-// } );
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+	response.send(`Thanks for subscribing, ${response.userProfile.name}`)
+} );
 
 
-// http.createServer(bot.middleware()).listen(4337, function(){
-// 	bot.setWebhook(webhookUrl)
-// });
+http.createServer(bot.middleware()).listen(4337, function(){
+	bot.setWebhook(webhookUrl)
+});
 
 
 
-// bot.on(BotEvents.SUBSCRIBED, response =>
-//     response.send(`Thanks for subscribing, ${response.userProfile.name}`));
+bot.on(BotEvents.SUBSCRIBED, response =>
+    response.send(`Thanks for subscribing, ${response.userProfile.name}`));
 // request.post({
 // 	url:'https://chatapi.viber.com/pa/send_message', 
 // 	data: {
@@ -48,3 +48,32 @@ const app = express();
 // 	console.log(1111111111111111111111111111111)
 // 	console.log(body)
 // })
+
+
+/******************/
+// function findUrls(text) {
+//     var source = (text || '').toString();
+//     var urlArray = [];
+//     var url;
+//     var matchArray;
+
+//     // Regular expression to find FTP, HTTP(S) and email URLs.
+//     var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)/g;
+
+//     // Iterate through any URLs in the text.
+//     while ((matchArray = regexToken.exec(source)) !== null) {
+//         var token = matchArray[0];
+//         urlArray.push(token);
+//     }
+
+//     return urlArray;
+// }
+// $("#section_0").each(function (index, elem) {
+//     var mess = $(this).text();
+//     var urlArray = findUrls(mess);
+//     urlArray.forEach(function (url) {
+//         var temp = mess.split(url);
+//         mess = temp.join("<a href=\"" + url + "\">" + url + "</a>");
+//     });
+//     $("#section_0 p").html(mess);
+// });
