18e6df0dd36ff7f86e74d7a301e0f3c9a6a84235
KilloBITS <mr.kalinu@gmail.com>
2020-01-19 17:57:40 +0200

    asd

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true,
 	store: new MongoStore({
-		url: 'mongodb://localhost:27017/AZIMUT'
-		// url: 'mongodb://AZIMUTSITE:qazwsx@localhost:27017/AZIMUT'
+		// url: 'mongodb://localhost:27017/AZIMUT'
+		url: 'mongodb://AZIMUTSITE:qazwsx@localhost:27017/AZIMUT'
 	}),
 	cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // two weeks
@@ -272,8 +272,8 @@ app.listen(4334,function(){
 	fs.readFile('./settings.json', 'utf8', function(err, settings_file) {
 		var params = JSON.parse(settings_file);
 	    global.baseName = "AZIMUT";
-		// global.baseIP = 'mongodb://admin:!QAZ2wsx@localhost:27017/';
-		global.baseIP = 'mongodb://localhost:27017/';
+		global.baseIP = 'mongodb://admin:!QAZ2wsx@localhost:27017/';
+		// global.baseIP = 'mongodb://localhost:27017/';
 		global.online = 0;
 
 		var defaultCollections = ['COMMENTS','CONFIG','LOGS','MARKET','NEWS','USERS','sessions','MESSAGE'];
